f8b0601586fc94fa2406787482e6bcb27f75b970
HarisHerdiansyah <haris54237@gmail.com>
2024-09-18 17:10:34 +0700

    feat: tugas 4

---
+++ tugas-004/tugas4.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int row;
+  cin >> row;
+
+  // * ===================================================
+  // *  ASTERISK
+  // * ===================================================
+
+  // for (int i = 1; i <= row; i++) {
+  //   for (int j = 1; j <= i; j++) {
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // int i = 1;
+  // while (i <= row) {
+  //   int j = 1;
+  //   while (j <= i) {
+  //     cout << "* ";
+  //     j++;
+  //   }
+  //   cout << endl;
+  //   i++;
+  // }
+
+  // for (int i = row; i >= 1; i--) {
+  //   for (int j = 1; j <= i; j++) {
+  //     cout << "* ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // int i = row;
+  // while (i >= 1) {
+  //   int j = 1;
+  //   while (j <= i) {
+  //     cout << "* ";
+  //     j++;
+  //   }
+  //   cout << endl;
+  //   i--;
+  // }
+
+  // * ===================================================
+  // *  NUMBER
+  // * ===================================================
+
+  // for (int i = 1; i <= row; i++) {
+  //   for (int j = 1; j <= i; j++) {
+  //     cout << j << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // int i = 1;
+  // while (i <= row) {
+  //   int j = 1;
+  //   while (j <= i) {
+  //     cout << j << " ";
+  //     j++;
+  //   }
+  //   cout << endl;
+  //   i++;
+  // }
+
+  // for (int i = row; i >= 1; i--) {
+  //   for (int j = 1; j <= i; j++) {
+  //     cout << j << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // int i = row;
+  // while (i >= 1) {
+  //   int j = 1;
+  //   while (j <= i) {
+  //     cout << j << " ";
+  //     j++;
+  //   }
+  //   cout << endl;
+  //   i--;
+  // }
+
+  return 0;
+}
