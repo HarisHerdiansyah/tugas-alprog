8b2fec3c328d7d97de77cd2dca08f0851ca77007
HarisHerdiansyah <haris54237@gmail.com>
2024-09-18 16:51:00 +0700

    feat: tugas 2

---
+++ tugas-004/tugas2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  int bound;
+  cin >> bound;
+
+  cout << setprecision(3);
+
+  cout << setw(62) << setfill('-') << '\n';
+  cout << '|' << left << setw(14) << setfill(' ') << "X";
+  cout << '|' << left << setw(14) << setfill(' ') << "1/X";
+  cout << '|' << left << setw(14) << setfill(' ') << "X ^ 2";
+  cout << '|' << left << setw(14) << setfill(' ') << "X ^ 3" << '|';
+  cout << setw(62) << setfill('-') << '\n';
+
+  for (int i = 1; i <= bound; i++) {
+    cout << '\n';
+    cout << '|' << left << setw(14) << setfill(' ') << i;
+    cout << '|' << left << setw(14) << setfill(' ') << (1.0 / i);
+    cout << '|' << left << setw(14) << setfill(' ') << pow(i, 2);
+    cout << '|' << left << setw(14) << setfill(' ') << pow(i, 3) << '|';
+  }
+
+  cout << setw(62) << setfill('-') << '\n';
+
+  return 0;
+}
